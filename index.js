ee0ed339305f10b6cac4368faee731bcdebbb56c
chaeplin <chaeplin@gmail.com>
2016-07-31 17:26:44 +0900

    add brightness/hue/saturation
    checking topic and length brightness/hue/saturation

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 // MQTT Switch Accessory plugin for HomeBridge
 //
 // Remember to add accessory to config.json. Example:
-// "accessories": [
-//     {
-//            "accessory": "mqttlightbulb",
-//            "name": "PUT THE NAME OF YOUR SWITCH HERE",
-//            "url": "PUT URL OF THE BROKER HERE",
-//			  "username": "PUT USERNAME OF THE BROKER HERE",
-//            "password": "PUT PASSWORD OF THE BROKER HERE"
-// 			  "caption": "PUT THE LABEL OF YOUR SWITCH HERE",
-// 			  "topics": {
-// 				"statusGet": 	"PUT THE MQTT TOPIC FOR THE GETTING THE STATUS OF YOUR SWITCH HERE",
-// 				"statusSet": 	"PUT THE MQTT TOPIC FOR THE SETTING THE STATUS OF YOUR SWITCH HERE"
-// 			  }
-//     }
-// ],
-//
 // When you attempt to add a device, it will ask for a "PIN code".
 // The default code for all HomeBridge accessories is 031-45-154.
 
@@ -26,66 +11,130 @@ var mqtt = require("mqtt");
 
 
 function mqttlightbulbAccessory(log, config) {
-  	this.log          	= log;
-  	this.name 		= config["name"];
-  	this.url 		= config["url"];
-	this.client_Id 		= 'mqttjs_' + Math.random().toString(16).substr(2, 8);
-	this.options = {
-	    keepalive: 10,
-    	    clientId: this.client_Id,
-	    protocolId: 'MQTT',
-    	    protocolVersion: 4,
-    	    clean: true,
-    	    reconnectPeriod: 1000,
-    	    connectTimeout: 30 * 1000,
-	    will: {
-			topic: 'WillMsg',
-			payload: 'Connection Closed abnormally..!',
-			qos: 0,
-			retain: false
-		},
-	    username: config["username"],
-	    password: config["password"],
-    	    rejectUnauthorized: false
-	};
-	this.caption = config["caption"];
-        this.topics  = config["topics"];
-	this.on = false;
-        this.brightness = 0;
-        this.hue = 0;
-        this.saturation = 0;
+  this.log          = log;
+  this.name         = config["name"];
+  this.url          = config["url"];
+  this.client_Id  = 'mqttjs_' + Math.random().toString(16).substr(2, 8);
+  this.options    = {
+      keepalive: 10,
+      clientId: this.client_Id,
+      protocolId: 'MQTT',
+      protocolVersion: 4,
+      clean: true,
+      reconnectPeriod: 1000,
+      connectTimeout: 30 * 1000,
+      will: {
+           topic: 'WillMsg',
+           payload: 'Connection Closed abnormally..!',
+           qos: 0,
+           retain: false
+        },
+      username: config["username"],
+      password: config["password"],
+      rejectUnauthorized: false
+  };
+  this.caption    = config["caption"];
+  this.topics     = config["topics"];
+  this.on         = false;
+  this.control_brightness = false;
+  this.control_hue        = false;
+  this.control_saturation = false;
+  this.brightness = 0;
+  this.hue        = 0;
+  this.saturation = 0;
 
-	this.service = new Service.Lightbulb(this.name);
-  	this.service
-        .getCharacteristic(Characteristic.On)
-    	.on('get', this.getStatus.bind(this))
-    	.on('set', this.setStatus.bind(this));
+  if (this.topics.setBrightness) {
+    if (this.topics.setBrightness.length > 0) {
+      this.control_brightness = true;
+    }
+  }
+  if (this.topics.setHue) {
+    if (this.topics.setHue.length > 0) {
+      this.control_hue = true;
+    }
+  }
+  if (this.topics.setSaturation) {
+    if (this.topics.setSaturation.length > 0) {
+      this.control_saturation = true;
+    }
+  }
 
-	// connect to MQTT broker
-	this.client = mqtt.connect(this.url, this.options);
-	var that = this;
-	this.client.on('error', function (err) {
-		that.log('Error event on MQTT:', err);
-	});
+  this.service = new Service.Lightbulb(this.name);
+  this.service
+    .getCharacteristic(Characteristic.On)
+      .on('get', this.getStatus.bind(this))
+      .on('set', this.setStatus.bind(this));
+  if (this.control_brightness) {
+    this.service
+      .getCharacteristic(Characteristic.Brightness)
+        .on('get', this.getBrightness.bind(this))
+        .on('set', this.setBrightness.bind(this));
+  }
+  if (this.control_hue) {
+    this.service
+      .getCharacteristic(Characteristic.Hue)
+        .on('get', this.getHue.bind(this))
+        .on('set', this.setHue.bind(this));
+  }
+  if (this.control_saturation) {
+    this.service
+      .getCharacteristic(Characteristic.Saturation)
+        .on('get', this.getSaturation.bind(this))
+        .on('set', this.setSaturation.bind(this));
+  }
 
-	this.client.on('message', function (topic, message) {
-        // console.log(message.toString(), topic);
 
-		if (topic == that.topics.getOn) {
-			var status = message.toString();
-			that.on = (status == "true" ? true : false);
-		   	that.service.getCharacteristic(Characteristic.On).setValue(that.on, undefined, 'fromSetValue');
-		}
+  // connect to MQTT broker
+  this.client = mqtt.connect(this.url, this.options);
+  var that = this;
+  this.client.on('error', function (err) {
+      that.log('Error event on MQTT:', err);
+  });
 
-        });
-        this.client.subscribe(this.topics.getOn);
+  this.client.on('message', function (topic, message) {
+  // console.log(message.toString(), topic);
+
+    if (topic == that.topics.getOn) {
+        var status = message.toString();
+        that.on = (status == "true" ? true : false);
+           that.service.getCharacteristic(Characteristic.On).setValue(that.on, undefined, 'fromSetValue');
+    }
+
+    if (topic == that.topics.getBrightness && this.control_brightness) {
+      var val = parseInt(message.toString());
+      that.brightness = val;
+      that.service.getCharacteristic(Characteristic.Brightness).setValue(that.brightness, undefined, 'fromSetValue');
+    }
+
+    if (topic == that.topics.getHue && this.control_hue) {
+      var val = parseInt(message.toString());
+      that.hue = val;
+      that.service.getCharacteristic(Characteristic.Hue).setValue(that.hue, undefined, 'fromSetValue');
+    }
+
+    if (topic == that.topics.getSaturation && this.control_saturation) {
+      var val = parseInt(message.toString());
+      that.saturation = val;
+      that.service.getCharacteristic(Characteristic.Brightness).setValue(that.saturation, undefined, 'fromSetValue');
+    }
+  });
+  this.client.subscribe(this.topics.getOn);
+  if (this.control_brightness) {
+    this.client.subscribe(this.topics.getBrightness);
+  }
+  if (this.control_hue) {
+    this.client.subscribe(this.topics.getHue);
+  }
+  if (this.control_saturation) {
+    this.client.subscribe(this.topics.getSaturation);
+  }
 }
 
 module.exports = function(homebridge) {
-  	Service = homebridge.hap.Service;
-  	Characteristic = homebridge.hap.Characteristic;
+      Service = homebridge.hap.Service;
+      Characteristic = homebridge.hap.Characteristic;
 
-  	homebridge.registerAccessory("homebridge-mqttlightbulb", "mqttlightbulb", mqttlightbulbAccessory);
+      homebridge.registerAccessory("homebridge-mqttlightbulb", "mqttlightbulb", mqttlightbulbAccessory);
 }
 
 mqttlightbulbAccessory.prototype.getStatus = function(callback) {
@@ -93,11 +142,50 @@ mqttlightbulbAccessory.prototype.getStatus = function(callback) {
 }
 
 mqttlightbulbAccessory.prototype.setStatus = function(status, callback, context) {
-	if(context !== 'fromSetValue') {
-		this.on = status;
-	  this.client.publish(this.topics.setOn, status ? "true" : "false");
-	}
-	callback();
+    if(context !== 'fromSetValue') {
+        this.on = status;
+      this.client.publish(this.topics.setOn, status ? "true" : "false");
+    }
+    callback();
+}
+
+mqttlightbulbAccessory.prototype.getBrightness = function(callback) {
+    callback(null, this.brightness);
+}
+
+mqttlightbulbAccessory.prototype.setBrightness = function(brightness, callback, context) {
+    if(context !== 'fromSetValue') {
+        this.brightness = brightness;
+    // console.log("Brightness:",this.brightness);
+      this.client.publish(this.topics.setBrightness, this.brightness.toString());
+    }
+    callback();
+}
+
+mqttlightbulbAccessory.prototype.getHue = function(callback) {
+    callback(null, this.hue);
+}
+
+mqttlightbulbAccessory.prototype.setHue = function(hue, callback, context) {
+    if(context !== 'fromSetValue') {
+        this.hue = hue;
+    // console.log("Hue:",this.hue);
+      this.client.publish(this.topics.setHue, this.hue.toString());
+    }
+    callback();
+}
+
+mqttlightbulbAccessory.prototype.getSaturation = function(callback) {
+    callback(null, this.saturation);
+}
+
+mqttlightbulbAccessory.prototype.setSaturation = function(saturation, callback, context) {
+    if(context !== 'fromSetValue') {
+        this.saturation = saturation;
+    // console.log("Saturation:",this.saturation);
+      this.client.publish(this.topics.setSaturation, this.saturation.toString());
+    }
+    callback();
 }
 
 mqttlightbulbAccessory.prototype.getServices = function() {
